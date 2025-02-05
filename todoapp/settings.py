b4e47a14493e70dc35a2fed87728f393257012e5
shakibosd <syednazmusshakib94@gmail.com>
2025-02-06 02:28:35 +0600

    finish 1

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True                                  
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/']
+CSRF_TRUSTED_ORIGINS = ['https://crud-operation-backends.onrender.com']
 
 CORS_ALLOW_ALL_ORIGINS = True   
 
